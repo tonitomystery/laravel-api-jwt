08380ad89642e8fef701fe1516950c1e7ef03c3f
tonitomystery <amaristany01@gmail.com>
2024-05-15 11:44:09 -0400

    fix add providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
+    App\Providers\EventServiceProvider::class,
+    Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 ];
