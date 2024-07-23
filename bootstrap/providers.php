aea95c6e63937a8607b3fa04ce9e5baab190ac94
tonitomystery <amaristany01@gmail.com>
2024-07-23 14:24:30 -0400

    feat: add repository provider to config

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,5 +5,6 @@ return [
     App\Providers\AuthServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
     App\Providers\EventServiceProvider::class,
+    App\Providers\RepositoryServiceProvider::class,
     Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 ];
